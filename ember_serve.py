c74c4e29a27270d632a48a5ba69b8ae297720a37
Jackson Dean <jckson.dean@gmail.com>
2016-10-24 12:42:04 -0700

    fix serve command

--- ember_serve.py
+++ ember_serve.py
@@ -6,19 +6,18 @@ class EmberServeCommand(sublime_plugin.WindowCommand):
     variables = self.window.extract_variables()
     if "folder" in variables:
       folder = variables["folder"]
-      shell_cmd = " ".join(["ember", "serve"])
       settings = sublime.load_settings('ember-tool-box.sublime-settings')
       linkedin = settings.get("linkedin", False);
 
       if linkedin:
         self.window.run_command("exec", {
-          "shell_cmd": "just " + shell_cmd,
+          "shell_cmd": "just serve",
           "working_dir": folder,
           "path": "/usr/local/linkedin/bin/"
         })
       else:
         self.window.run_command("exec", {
-          "shell_cmd": shell_cmd,
+          "shell_cmd": "ember serve",
           "working_dir": folder,
           "path": settings.get("ember_path", "/usr/local/bin/ember")
         })
